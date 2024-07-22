542842f714a39483b9ee00935bca7c1c4176301f
Abdoulaye Alhassane Diallo <62873841+Kienda@users.noreply.github.com>
2024-07-22 11:39:29 -0400

    protype methede to class

--- starterjs/index.js
+++ starterjs/index.js
@@ -1,1 +1,22 @@
-alert("Hello world")
+//constructor function
+
+function Person(firstname, lastname, dob) {
+    // to access the value of the object we the keyword this?
+
+    this.firstname = firstname
+    this.lastname = lastname
+    this.dob = new Date(dob)
+
+    // we can create function inside the constructor
+
+    this.getBirthYear = function () { return this.dob.getFullYear(); }
+    this.geFullName = function () { return `${this.firstName} ${this.lastName}` }
+
+
+
+}
+
+// can set prototype to the constructor properties for more readable way 
+
+Person.prototype.getBirthYear = function () { return this.dob.getFullYear(); }
+Person.geFullName = function () { return `${this.firstName} ${this.lastName}` }
